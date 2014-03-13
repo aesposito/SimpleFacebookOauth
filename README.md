1442f098da46cc52ef829e0c770f8e1e53527d4e
aesposito <aesposito@innvenio.com>
2014-03-13 01:00:37 -0300

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,67 @@ SimpleFacebookOauth
 ===================
 
 Simple javascript facebook oauth
+
+<br/>
+<strong>Example:</strong>
+<br/><br/>
+
+```html
+
+<head>
+	<script type="text/javascript" src="https://connect.facebook.net/en_US/all.js"></script>
+	<!-- Replace YOUR_APP_ID in SimpleFacebookOauth.js file-->
+	<script type="text/javascript" src="SimpleFacebookOauth.js"></script>
+</head>
+
+<html>
+...
+	<script>
+
+	var oauth = new SimpleFacebookOauth();
+
+	//Login Facebook oauth
+	oauth.login({scope:'email,publish_stream'}, function(success, token){
+		if (success)
+		{
+
+		}
+	});
+
+	//Publish in user's wall
+	oauth.publish({
+		message: "message",
+		picture: "http://...",
+		link: "http://..."
+	}, function(e){
+
+	});
+
+	//User info
+	oauth.getMe(function(me){
+		//me.name
+		//me.first_name
+		//me.last_name
+		//me.email
+		//me.picture
+	});
+
+	</script>
+
+...
+</html>
+
+```
+
+<br/>
+<strong>Functions</strong>
+<br/><br/>
+
+* Facebook Oauth
+* Get user info
+* Publish Stream
+* Check login status
+
+<h2>Changelog</h2>
+#### 1.0<br/>
+* SimpleFacebookOauth
---
+++ SimpleFacebookOauth.js
@@ -0,0 +1,60 @@
+window.fbAsyncInit = function() {
+	FB.init({ 
+		appId: 'YOUR_APP_ID', 
+		status: true, 
+		cookie: true,
+		xfbml: true,
+		oauth: true
+	});
+};
+
+
+function SimpleFacebookOauth () {
+
+	this.login = function(scope, callback) {
+		FB.login(function(response) {
+			callback(response.authResponse, response.authResponse.accessToken);
+		}, scope);
+	};
+
+
+	this.getMe = function(callback)
+	{
+		FB.api('/me', function(response) {
+			callback({
+				id: response.id, 
+				name:response.name, 
+				first_name:response.first_name,
+				last_name:response.last_name, 
+				email:response.email, 
+				picture: "https://graph.facebook.com/" + response.id + "/picture/?type=large"
+			});	
+
+		});
+	};
+
+	this.publish = function(options, callback){
+		FB.api('/me/feed', 'POST', options, function(response) {
+			callback();
+		})
+	};
+
+	this.checkLogin = function(callback)
+	{
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				callback({
+					success: true, 
+					id: response.authResponse.userID, 
+					token: response.authResponse.accessToken
+				});
+			} 
+			else
+			{
+				callback({
+					success: false
+				});
+			}		
+		});
+	}
+}
